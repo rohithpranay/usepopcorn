8d9feec248b72d3a077548793f892e9daaa0f626
rohithpranay <pranayrohith@gmail.com>
2024-04-26 23:26:48 +0530

    added use effect hook and implemented watched list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={5} defaultRating={2} />
+    <App />
   </React.StrictMode>
 );
